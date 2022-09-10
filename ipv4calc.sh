33a7c9ef74716f9bc4742551f977615cad69a486
sysad.sacks <sysad.sacks@gmail.com>
2022-09-09 20:50:48 -0400

    getting bash rematch working

--- ipv4calc.sh
+++ ipv4calc.sh
@@ -5,19 +5,32 @@
 # To run the script, the syntax should be: ./ipv4calc.sh [IP address] [subnet mask]
 # 1st argument [$1] = IP address
 # 2nd argument [$2]= subnet mask
+[[ "$2" =~ ([0-9]+).([0-9]+).([0-9]+).([0-9]+) ]]
+
 target="$1 $2"
 network=0
-#
+mask=0
 
-# for i in ${filenamearray[@]}; do
-#   [[ "$i" =~ (${user})([0-9]+) ]]
-#   # echo "${BASH_REMATCH[2]}"
-#   filenumberarray+=(${BASH_REMATCH[2]})
-# done
+for i in ${BASH_REMATCH[@]}; do
+    if [[ $i == 255 ]]; then
+        (( mask += 8 ))
+        echo $i
+    elif [[ $i > 0 && $i < 255 ]]; then
+        echo interesting
+    elif [[ $i == 0 ]]; then
+        echo "network equals 0"
+    fi
+done
+echo $mask
 
 # break up the IP address into octets
 [[ "$1" =~ ([0-9]+).([0-9]+).([0-9]+).([0-9]+) ]]
-echo "${BASH_REMATCH[*]}"
+ip1=${BASH_REMATCH[1]}
+ip2=${BASH_REMATCH[2]}
+ip3=${BASH_REMATCH[3]}
+ip4=${BASH_REMATCH[4]}
+
+# echo $ip4
 
 
 # function to process each octet
