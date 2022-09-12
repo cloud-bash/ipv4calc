6b2a3f849863722a7e5a804b987750a0be139458
sysad.sacks <sysad.sacks@gmail.com>
2022-09-12 17:20:04 -0400

    got the loop working

--- ipv4calc.sh
+++ ipv4calc.sh
@@ -7,50 +7,84 @@
 # 2nd argument [$2]= subnet mask
 
 # initialize variables for later use
-target=()
-network=""
-cidr=0
 
-# break up the IP address into octets
+# given random ip address + subnet mask (taken from the user arguments $1 and $2)
+target=()
+# network address for our mystery IP address
+network=()
+# will be calculated based on subnet mask
+cidr=0
+# x=octet index counter
+x=0
+
+# break up the IP address into a list of octets
 [[ "$1" =~ ([0-9]+).([0-9]+).([0-9]+).([0-9]+) ]]
 for i in ${BASH_REMATCH[@]:1:4}; do
     target+=("$i")
 done
 
-echo ${target[@]}
-
-# ${arr[@]:s:n}	Retrieve n elements starting at index s
 # get CIDR from subnet mask using regex groups
-# [[ "$2" =~ ([0-9]+).([0-9]+).([0-9]+).([0-9]+) ]]
+[[ "$2" =~ ([0-9]+).([0-9]+).([0-9]+).([0-9]+) ]]
 # ${arr[@]:s:n}	Retrieve n elements starting at index s
-# for i in ${BASH_REMATCH[@]:1:4}; do
-#     echo $i
-#     x=0
-#     if [[ $i == 255 ]]; then
-
-#         (( cidr += 8 ))
-#         network+="${ip[x]}"
-#         (( x++ ))
-#         echo $x
-#     elif [[ $i > 0 && $i < 255 ]]; then
-#         network+="?"
-#         # echo interesting
-#     elif [[ $i == 0 ]]; then
-#         network+="0"
-#         echo "network octet equals 0"
-#     fi
-# done
-# echo $mask
-# echo $network
-
-# echo "Network Address: $network"
-# echo "First Host Address: $network"
+for i in ${BASH_REMATCH[@]:1:4}; do
+    # if this mask octet is 255,
+    if [[ $i == 255 ]]; then
+        # the network matches our target for this octet
+        # append this octet to the network address variable
+        network+=("${target[x]}")
+        # 8 bits added to cidr
+        (( cidr+=8 ))
+        # increment octet index counter
+        (( x++ ))
+    elif [[ $i > 0 && $i < 255 ]]; then
+        # m - magic number
+        (( m=256-i ))
+        # bash will use floored division automatically bc integer data type
+        # d = floor divide current octet of target by the magic number
+        d=$(( ${target[$x]}/m ))
+        # z = network value for this octet
+        z=$(( d*m ))
+        # add z to network octet list
+        network+=("$z")
+        # increment octet index counter
+        (( x++ ))
+        # add correct number of bits for cidr
+        # 128, 192, 224, 240, 248, 252, 254
+        # oct - 128 > 0 then cidr+=1...etc.
+        case $i in
+            "128")
+                (( cidr+=1 ))
+                ;;
+            "192")
+                (( cidr+=2 ))
+                ;;
+            "224")
+                (( cidr+=3 ))
+                ;;
+            "240")
+                (( cidr+=4 ))
+                ;;
+            "248")
+                (( cidr+=5 ))
+                ;;
+            "252")
+                (( cidr+=6 ))
+                ;;
+            "254")
+                (( cidr+=7 ))
+                ;;
+        esac
+    elif [[ $i == 0 ]]; then
+        network+=("0")
+    fi
+done
+echo "Target Address: ${target[@]}"
+echo "CIDR: /"$cidr
+echo "Network Address: ${network[@]}"
+echo "First Host Address: ${network[@]:0:3} $(( ${network[3]}+1 ))"
 # echo "Last Host Address: $network"
 # echo "Broadcast Address: $network"
 # echo "Next Network Address: $network"
-
-
-# # function to process each octet
 
 # # if this octet is 255, this octet in the network address
 # # is equal to the corresponding octet of the given IP address
@@ -64,11 +98,3 @@ echo ${target[@]}
 # # else
 # #     network="interesting"
 # # fi
-
-# # echo "Your network octet is $network"
-
-# # for i in ${filenamearray[@]}; do
-# #   [[ "$i" =~ (${user})([0-9]+) ]]
-# #   # echo "${BASH_REMATCH[2]}"
-# #   filenumberarray+=(${BASH_REMATCH[2]})
-# # done
